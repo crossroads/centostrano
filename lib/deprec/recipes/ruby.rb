535fa0fa4ad8c9b79bcf6e13ac61905253e1adc4
warp <ben.tillman@gmail.com>
2010-10-19 22:12:56 -0700

    Adjusted ruby install for centos

--- lib/deprec/recipes/ruby.rb
+++ lib/deprec/recipes/ruby.rb
@@ -7,20 +7,20 @@ Capistrano::Configuration.instance(:must_exist).load do
       SRC_PACKAGES[:mri] = {
         :md5sum => "755aba44607c580fddc25e7c89260460  ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz", 
         :url => "ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz",
-        :configure => "./configure --with-readline-dir=/usr/local;"
+        :configure => "./configure --with-install-readline;"
       }
       
       desc "Install Ruby"
       task :install do
         install_deps
         deprec2.download_src(SRC_PACKAGES[:mri], src_dir)
-        deprec2.install_from_src(SRC_PACKAGES[:mri], src_dir)
+        yum.install_from_src(SRC_PACKAGES[:mri], src_dir)
         # Ruby versions from 1.9.1 install rubygems
         # top.deprec.rubygems.install
       end
       
       task :install_deps do
-        apt.install( {:base => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline5-dev)}, :stable )
+        apt.install( {:base => %w(pcre* gcc make openssl openssl-devel readline-devel)}, :stable )
       end
       
     end
@@ -33,8 +33,8 @@ Capistrano::Configuration.instance(:must_exist).load do
       SRC_PACKAGES[:rubygems] = {
         :md5sum => "e85cfadd025ff6ab689375adbf344bbe  rubygems-1.3.7.tgz", 
         :url => "http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz",
-	    :configure => "",
-	    :make =>  "",
+	:configure => "",
+	:make => "",
         :install => 'ruby setup.rb;'
       }
       
@@ -42,12 +42,13 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :install do
         install_deps
         deprec2.download_src(SRC_PACKAGES[:rubygems], src_dir)
-        deprec2.install_from_src(SRC_PACKAGES[:rubygems], src_dir)
+        yum.install_from_src(SRC_PACKAGES[:rubygems], src_dir)
         # gem2.upgrade #  you may not want to upgrade your gems right now
       end
       
       # install dependencies for rubygems
       task :install_deps do
+        apt.install( {:base => %w(pcre* gcc make openssl openssl-devel)}, :stable )
       end
       
     end 
