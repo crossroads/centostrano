c3841cb398dc3e05cef21db64638cbb037d2c13c
warp <ben.tillman@gmail.com>
2010-10-19 22:04:30 -0700

    Switched ruby version to 1.9.2-p0

--- lib/deprec/recipes/ruby.rb
+++ lib/deprec/recipes/ruby.rb
@@ -3,66 +3,51 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :centos do
     namespace :ruby do
-            
-      SRC_PACKAGES[:ruby] = {
-        :filename => 'ruby-1.8.6-p111.tar.gz',   
-        :md5sum => "c36e011733a3a3be6f43ba27b7cd7485 ruby-1.8.6-p111.tar.gz", 
-        :dir => 'ruby-1.8.6-p111',  
-        :url => "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p111.tar.gz",
-        :unpack => "tar zxf ruby-1.8.6-p111.tar.gz; cd ruby-1.8.6-p111; wget -q -O - wget http://blog.phusion.nl/assets/r8ee-security-patch-20080623-2.txt | patch -p1;",
-        :configure => %w(
-          ./configure
-          --with-install-readline
-          ;
-          ).reject{|arg| arg.match '#'}.join(' '),
-        :make => 'make;',
-        :install => 'make install;',
-        :version => 'c1.8.6',
-        :release => '111eephusion'
+      
+      SRC_PACKAGES[:mri] = {
+        :md5sum => "755aba44607c580fddc25e7c89260460  ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz", 
+        :url => "ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz",
+        :configure => "./configure --with-readline-dir=/usr/local;"
       }
-  
+      
+      desc "Install Ruby"
       task :install do
         install_deps
-        deprec2.download_src(SRC_PACKAGES[:ruby], src_dir)
-        yum.install_from_src(SRC_PACKAGES[:ruby], src_dir)
+        deprec2.download_src(SRC_PACKAGES[:mri], src_dir)
+        deprec2.install_from_src(SRC_PACKAGES[:mri], src_dir)
+        # Ruby versions from 1.9.1 install rubygems
+        # top.deprec.rubygems.install
       end
       
       task :install_deps do
-        apt.install( {:base => %w(pcre* gcc make openssl openssl-devel readline-devel patch)}, :stable )
+        apt.install( {:base => %w(zlib1g-dev libssl-dev libncurses5-dev libreadline5-dev)}, :stable )
       end
-
+      
     end
   end
   
   
   namespace :centos do
     namespace :rubygems do
-  
+
       SRC_PACKAGES[:rubygems] = {
-        :filename => 'rubygems-1.3.1.tgz',   
-        :md5sum => "a04ee6f6897077c5b75f5fd1e134c5a9  rubygems-1.3.1.tgz", 
-        :url => "http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz",
-        :dir => 'rubygems-1.3.1',  
-        :unpack => "tar zxf rubygems-1.3.1.tgz;",
-        :install => 'ruby setup.rb;',
-        :version => 'c1.3.1',
-        :release => '1'
+        :md5sum => "e85cfadd025ff6ab689375adbf344bbe  rubygems-1.3.7.tgz", 
+        :url => "http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz",
+	    :configure => "",
+	    :make =>  "",
+        :install => 'ruby setup.rb;'
       }
       
+      desc "Install Rubygems"
       task :install do
         install_deps
         deprec2.download_src(SRC_PACKAGES[:rubygems], src_dir)
-        yum.install_from_src(SRC_PACKAGES[:rubygems], src_dir)
+        deprec2.install_from_src(SRC_PACKAGES[:rubygems], src_dir)
         # gem2.upgrade #  you may not want to upgrade your gems right now
-        # If we want to selfupdate then we need to 
-        # create symlink as latest gems version is broken
-        # gem2.update_system
-        # sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
       end
       
       # install dependencies for rubygems
       task :install_deps do
-        apt.install( {:base => %w(pcre* gcc make openssl openssl-devel)}, :stable )
       end
       
     end 
