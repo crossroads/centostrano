41a0050ec93139653209b850481e2543f84807d4
warp <ben.tillman@gmail.com>
2010-10-20 19:09:55 -0700

    Use passenger gem rather than source build

--- lib/deprec/recipes/passenger.rb
+++ lib/deprec/recipes/passenger.rb
@@ -2,21 +2,15 @@
 Capistrano::Configuration.instance(:must_exist).load do 
   namespace :centos do 
     namespace :passenger do
-      
-      set(:passenger_install_dir) { 
-        if passenger_use_ree
-          "#{ree_install_dir}/lib/ruby/gems/1.8/gems/passenger-2.0.6"
-        else
-          '/opt/passenger'
-        end
-      }
-      
 
+      set :passenger_version, '2.2.14'    
+      set :passenger_install_dir, "/usr/local/lib/ruby/gems/1.8/gems/passenger-#{passenger_version}"
+   
+      # Default settings for Passenger config files
       set(:passenger_document_root) { "#{current_path}/public" }
       set :passenger_rails_allow_mod_rewrite, 'off'
-      set :passenger_vhost_dir, '/usr/local/apache2/conf/apps'
-      # Default settings for Passenger config files
       set :passenger_log_level, 0
+      set(:passenger_log_dir) { "#{shared_path}/log"} 
       set :passenger_user_switching, 'on'
       set :passenger_default_user, 'nobody'
       set :passenger_max_pool_size, 6
@@ -25,76 +19,47 @@ Capistrano::Configuration.instance(:must_exist).load do
       set :passenger_rails_autodetect, 'on'
       set :passenger_rails_spawn_method, 'smart' # smart | conservative
 
-      SRC_PACKAGES[:passenger] = {
-        :url => "git://github.com/FooBarWidget/passenger.git",
-        :download_method => :git,
-        :version => 'release-2.0.6', # Specify a tagged release to deploy
-        :configure => '',
-        :make => '',
-        :install => ' ./bin/passenger-install-apache2-module'
-      }
+      desc "Install passenger"
+      task :install, :roles => :app do
+        install_deps
+        gem2.install 'passenger', passenger_version
+        sudo "passenger-install-apache2-module _#{passenger_version}_ -a"
+        config_system
+        activate_system 
+      end
+      
+      # Install dependencies for Passenger
+      task :install_deps, :roles => :app do
+        apt.install( {:base => %w(rsync apr-devel)}, :stable )
+      end
       
       SYSTEM_CONFIG_FILES[:passenger] = [
-      # Hmm...we need to place those non-app config files somewhere else - Saulius
+
         {:template => 'passenger.load.erb',
-          :path => '/usr/local/apache2/conf/apps/passenger.load',
+          :path => '/etc/apache2/mods-available/passenger.load',
           :mode => 0755,
           :owner => 'root:root'},
-
+          
         {:template => 'passenger.conf.erb',
-          :path => '/usr/local/apache2/conf/apps/passenger.conf',
+          :path => '/etc/apache2/mods-available/passenger.conf',
           :mode => 0755,
           :owner => 'root:root'}
-
 
       ]
 
       PROJECT_CONFIG_FILES[:passenger] = [
 
         { :template => 'apache_vhost.erb',
-          :path => 'apache_vhost',
+          :path => "apache_vhost",
           :mode => 0755,
-          :owner => 'root:root'}
+          :owner => 'root:root'},
+          
+        {:template => 'logrotate.conf.erb',
+         :path => "logrotate.conf", 
+         :mode => 0644,
+         :owner => 'root:root'}
 
       ]
-
-      desc "Install passenger"
-      task :install, :roles => :web do
-        install_deps
-        deprec2.download_src(SRC_PACKAGES[:passenger], src_dir)
-
-        if passenger_use_ree
-          # needs porting to CentOS - Saulius
-          # Install the Passenger that came with Ruby Enterprise Edition
-          run "yes | #{sudo} env PATH=#{ree_install_dir}/bin:$PATH #{ree_install_dir}/bin/passenger-install-apache2-module"
-        else
-          package_dir = File.join(src_dir, 'passenger.git')
-          dest_dir = passenger_install_dir + '-' + (SRC_PACKAGES[:passenger][:version] || 'trunk')
-          run "#{sudo} rsync -avz #{package_dir}/ #{dest_dir}"
-          sudo "su -c 'export PATH=/usr/local/apache2/bin:$PATH && export APXS2=/usr/local/apache2/bin/apxs && export APR_CONFIG=/usr/local/apache2/bin/apr-1-config && cd #{dest_dir} && yes '' | ./bin/passenger-install-apache2-module'"
-          #run "cd #{dest_dir} && #{sudo} ./bin/passenger-install-apache2-module"
-          run "#{sudo} unlink #{passenger_install_dir} 2>/dev/null; #{sudo} ln -sf #{dest_dir} #{passenger_install_dir}"
-        end
-          
-        initial_config_push
-      end
-
-      task :initial_config_push, :roles => :web do
-        # XXX Non-standard!
-        # We need to push out the .load and .conf files for Passenger
-        SYSTEM_CONFIG_FILES[:passenger].each do |file|
-          deprec2.render_template(:passenger, file.merge(:remote => true))
-        end
-      end
-
-      # install dependencies for nginx
-      task :install_deps, :roles => :web do
-        apt.install( {:base => %w(rsync apr-devel)}, :stable )
-        gem2.install 'fastthread'
-        gem2.install 'rack'
-        gem2.install 'rake'
-        top.centos.apache.install
-      end
        
       desc "Generate Passenger apache configs (system & project level)."
       task :config_gen do
@@ -131,12 +96,22 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "Push Passenger configs (project level) to server"
       task :config_project, :roles => :app do
         deprec2.push_configs(:passenger, PROJECT_CONFIG_FILES[:passenger])
-        symlink_passenger_vhost
+        symlink_apache_vhost
         activate_project
+        symlink_logrotate_config
       end
-
-      task :symlink_passenger_vhost, :roles => :app do
-        sudo "ln -sf #{deploy_to}/passenger/apache_vhost #{passenger_vhost_dir}/#{application}.conf"
+      
+      task :symlink_logrotate_config, :roles => :app do
+        sudo "ln -sf #{deploy_to}/passenger/logrotate.conf /etc/logrotate.d/passenger-#{application}"
+      end
+      
+      # Passenger runs Rails as the owner of this file.
+      task :set_owner_of_environment_rb, :roles => :app do
+        sudo "chown #{app_user} #{current_path}/config/environment.rb"
+      end
+      
+      task :symlink_apache_vhost, :roles => :app do
+        sudo "ln -sf #{deploy_to}/passenger/apache_vhost #{apache_vhost_dir}/#{application}"
       end
       
       task :activate, :roles => :app do
@@ -145,13 +120,13 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
       
       task :activate_system, :roles => :app do
-        #sudo "a2enmod passenger"
-        top.centos.web.reload
+        sudo "a2enmod passenger"
+        top.deprec.web.reload
       end
       
       task :activate_project, :roles => :app do
-        #sudo "a2ensite #{application}"
-        top.centos.web.reload
+        sudo "a2ensite #{application}"
+        top.deprec.web.reload
       end
       
       task :deactivate do
@@ -160,74 +135,36 @@ Capistrano::Configuration.instance(:must_exist).load do
         puts
         puts "Danger!"
         puts
-        puts
         puts "Do you want to deactivate just this project or all Passenger"
         puts "projects on this server? Try a more granular command:"
         puts
-        puts "cap centos:passenger:deactivate_system  # disable Passenger"
-        puts "cap centos:passenger:deactivate_project # disable only this project"
+        puts "cap deprec:passenger:deactivate_system  # disable Passenger"
+        puts "cap deprec:passenger:deactivate_project # disable only this project"
         puts
         puts "******************************************************************"
         puts
       end
       
       task :deactivate_system, :roles => :app do
-        #sudo "a2dismod passenger"
-        top.centos.web.reload
+        sudo "a2dismod passenger"
+        top.deprec.web.reload
       end
       
       task :deactivate_project, :roles => :app do
-        #sudo "a2dissite #{application}"
-        top.centos.web.reload
+        sudo "a2dissite #{application}"
+        top.deprec.web.reload
       end
       
       desc "Restart Application"
       task :restart, :roles => :app do
-        run "touch #{current_path}/tmp/restart.txt"
+        run "#{sudo} touch #{current_path}/tmp/restart.txt"
       end
       
       desc "Restart Apache"
-      task :restart_apache, :roles => :web do
-        run "#{sudo} /etc/init.d/httpd restart"
+      task :restart_apache, :roles => :passenger do
+        run "#{sudo} /etc/init.d/apache2 restart"
       end
       
-      namespace :ree do
-      # need to port to CentOS all that REE thing - Saulius
-        set :ree_version, 'ruby-enterprise-1.8.6-20090113'
-        set :ree_install_dir, "/opt/#{ree_version}"
-        set :ree_short_path, '/opt/ruby-enterprise'
-        
-        SRC_PACKAGES[:ree] = {
-          :md5sum => "e8d796a5bae0ec1029a88ba95c5d901d #{ree_version}.tar.gz",
-          :url => "http://rubyforge.org/frs/download.php/50087/#{ree_version}.tar.gz",
-          :configure => '',
-          :make => '',
-          :install => "./installer --auto /opt/#{ree_version}"
-        }
-   
-        task :install do
-          install_deps
-          deprec2.download_src(SRC_PACKAGES[:ree], src_dir)
-          deprec2.install_from_src(SRC_PACKAGES[:ree], src_dir)
-          symlink_ree
-        end
-        
-        task :install_deps do
-          apt.install({:base => %w(libssl-dev libmysqlclient15-dev libreadline5-dev)}, :stable)
-        end
-        
-        task :symlink_ree do
-          sudo "ln -sf /opt/#{ree_version} #{ree_short_path}"
-          sudo "ln -fs #{ree_short_path}/bin/gem /usr/local/bin/gem"
-          sudo "ln -fs #{ree_short_path}/bin/irb /usr/local/bin/irb"
-          sudo "ln -fs #{ree_short_path}/bin/rake /usr/local/bin/rake"
-          sudo "ln -fs #{ree_short_path}/bin/rails /usr/local/bin/rails"
-          sudo "ln -fs #{ree_short_path}/bin/ruby /usr/local/bin/ruby"
-        end
-        
-      end
-    
-
     end
   end
 end
