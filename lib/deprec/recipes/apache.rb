284ac18e2168b960056537c54f05453d7d03b312
warp <ben.tillman@gmail.com>
2010-10-20 19:26:05 -0700

    Use apache from repository rather than source

--- lib/deprec/recipes/apache.rb
+++ lib/deprec/recipes/apache.rb
@@ -3,52 +3,49 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :centos do
     namespace :apache do
       
-      SRC_PACKAGES[:apache] = {
-        :filename => 'httpd-2.2.17.tar.gz',   
-        :md5sum => "66d8e107f85acc039fd5e624e85728a9 *httpd-2.2.17.tar.gz", 
-        :dir => 'httpd-2.2.17',  
-        :url => "http://www.apache.org/dist/httpd/httpd-2.2.17.tar.gz",
-        :unpack => "tar zxf httpd-2.2.17.tar.gz;",
-        :configure => %w(
-          ./configure
-          --enable-mods-shared=all
-          --enable-proxy 
-          --enable-proxy-balancer 
-          --enable-proxy-http 
-          --enable-rewrite  
-          --enable-cache 
-          --enable-headers 
-          --enable-ssl 
-          --enable-deflate 
-          --with-included-apr   #_so_this_recipe_doesn't_break_when_rerun
-          --enable-dav          #_for_subversion_
-          --enable-so           #_for_subversion_
-          ;
-          ).reject{|arg| arg.match '#'}.join(' '),
-        :make => 'make;',
-        :install => '--fstrans=no make install;',
-        :post_install => 'install -b support/apachectl /etc/init.d/httpd;',
-        :version => 'c2.2.17',
-        :release => '1'
-      }
-
+      set :apache_user, 'www-data'
+      set :apache_vhost_dir, '/etc/httpd/sites-available'
+      set :apache_ssl_enabled, false
+      set :apache_ssl_ip, nil
+      set :apache_ssl_forward_all, apache_ssl_enabled
+      set :apache_ssl_chainfile, false
+      set :apache_modules_enabled, %w(rewrite ssl proxy_balancer proxy_http deflate expires headers)
+      set :apache_log_dir, '/var/log/apache2'
+       
       desc "Install apache"
       task :install do
         install_deps
-        sudo "yum remove -y httpd"
-        deprec2.download_src(SRC_PACKAGES[:apache], src_dir)
-        yum.install_from_src(SRC_PACKAGES[:apache], src_dir)
-        #enable_mod_rewrite
+        enable_modules
+        reload
       end
       
       # install dependencies for apache
       task :install_deps do
-        apt.install( {:base => %w(zlib1g-dev zlib1g openssl openssl-devel)}, :stable )
+        apt.install( {:base => %w(openssl openssl-devel zlib zlib-devel)}, :stable )
       end
       
       SYSTEM_CONFIG_FILES[:apache] = [
-        # They're generated and put in place during install
-        # I may put them in here at some point
+        
+        { :template => 'apache2.conf.erb',
+          :path => '/etc/apache2/apache2.conf',
+          :mode => 0644,
+          :owner => 'root:root'},
+
+        { :template => 'namevirtualhosts.conf',
+          :path => '/etc/apache2/conf.d/namevirtualhosts.conf',
+          :mode => 0644,
+          :owner => 'root:root'},
+          
+        { :template => 'ports.conf.erb',
+          :path => '/etc/apache2/ports.conf',
+          :mode => 0644,
+          :owner => 'root:root'},
+
+        { :template => 'status.conf.erb',
+          :path => '/etc/apache2/mods-available/status.conf',
+          :mode => 0644,
+          :owner => 'root:root'}
+          
       ]
 
       PROJECT_CONFIG_FILES[:apache] = [
@@ -70,53 +67,72 @@ Capistrano::Configuration.instance(:must_exist).load do
         PROJECT_CONFIG_FILES[:apache].each do |file|
           deprec2.render_template(:apache, file)
         end
+        # XXX Need to flesh out generation of custom certs
+        # In the meantime we'll just use the snakeoil cert
+        #
+        # top.deprec.ssl.config_gen if apache_ssl_enabled
+        top.deprec.ssl.config_gen if apache_ssl_enabled
       end
       
       desc "Push apache config files to server"
       task :config, :roles => :web do
+        config_system
+        reload
+      end
+      
+      desc "Generate initial configs and copy direct to server."
+      task :initial_config, :roles => :web do
+        SYSTEM_CONFIG_FILES[:apache].each do |file|
+          deprec2.render_template(:apache, file.merge(:remote => true))
+        end
+      end
+      
+      desc "Push apache config files to server"
+      task :config_system, :roles => :web do
         deprec2.push_configs(:apache, SYSTEM_CONFIG_FILES[:apache])
       end
-
-      # Stub so generic tasks don't fail (e.g. centos:web:config_project)
-      task :config_project, :roles => :web do
+      
+      # Stub so generic tasks don't fail (e.g. deprec:web:config_project)
+      task :config_project do
+        # XXX Need to flesh out generation of custom certs
+        # In the meantime we'll just use the snakeoil cert
+        #
+        top.deprec.ssl.config if apache_ssl_enabled
       end
-
-      task :enable_mod_rewrite, :roles => :web do
-        #sudo "a2enmod rewrite"
+      
+      task :enable_modules, :roles => :web do
+        apache_modules_enabled.each { |mod| sudo "a2enmod #{mod}" }
+        reload
       end
-
-
+      
       desc "Start Apache"
       task :start, :roles => :web do
-        send(run_method, "/etc/init.d/httpd start")
+        send(run_method, "/etc/init.d/apache2 start")
       end
 
       desc "Stop Apache"
       task :stop, :roles => :web do
-        send(run_method, "/etc/init.d/httpd stop")
+        send(run_method, "/etc/init.d/apache2 stop")
       end
 
       desc "Restart Apache"
       task :restart, :roles => :web do
-        send(run_method, "/etc/init.d/httpd restart")
+        send(run_method, "/etc/init.d/apache2 restart")
       end
 
       desc "Reload Apache"
       task :reload, :roles => :web do
-        send(run_method, "/etc/init.d/httpd reload")
+        send(run_method, "/etc/init.d/apache2 force-reload")
       end
 
       desc "Set apache to start on boot"
       task :activate, :roles => :web do
-        send(run_method, "sed -i '2i# chkconfig: 2345 10 90' /etc/init.d/httpd")
-        send(run_method, "sed -i '3i# description: Activates/Deactivates Apache Web Server' /etc/init.d/httpd")
-        send(run_method, "/sbin/chkconfig --add httpd")
-        send(run_method, "/sbin/chkconfig --level 345 httpd on")
+        send(run_method, "update-rc.d apache2 defaults")
       end
       
       desc "Set apache to not start on boot"
       task :deactivate, :roles => :web do
-        send(run_method, "/sbin/chkconfig --del httpd")
+        send(run_method, "update-rc.d -f apache2 remove")
       end
       
       task :backup, :roles => :web do
@@ -127,13 +143,6 @@ Capistrano::Configuration.instance(:must_exist).load do
         # not yet implemented
       end
 
-      # Generate an index.html page  
-      task :install_index_page do
-        deprec2.mkdir(apache_docroot, :owner => :root, :group => :deploy, :mode => 0775, :via => :sudo)
-        std.su_put deprec2.render_template(:apache, :template => 'index.html.erb'), File.join('/var/www/index.html')
-        std.su_put deprec2.render_template(:apache, :template => 'master.css'), File.join('/var/www/master.css')
-      end
-      
     end
   end
 end
